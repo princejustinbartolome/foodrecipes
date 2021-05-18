b4b50db017c93b52d0a253fd2bb8743c45fea18c
princejustin <pjdb.bartolome@gmail.com>
2021-05-18 12:25:26 +0800

    UI Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,16 @@ import '../App.css';
 import uuid from 'react-uuid';
 import Recipes from './Recipes';
 import Form from './Form';
+import Viewrecipe from './viewRecipe';
 
 const Home = () => {
   return (
     <div className="food-preview-block">
       <div className="food-preview-header">
         <p>Food Recipes</p>
-        <p><a data-toggle="modal" data-target="#myModal" href="#">Add Recipes / Save Recipes</a></p>
+        <p><a data-toggle="modal" data-target="#myModal" href="#">Add Recipe / Save Recipe</a></p>
       </div>
-      <Form />
+      <Recipes />
     </div>
   )
 }
---
+++ src/components/viewRecipe.js
@@ -0,0 +1,105 @@
+const viewRecipe = () => {
+  
+    return (
+      <>
+      <div className="view-food-recipe-area">
+        <div className="vfr-block">
+          <div className="vfr-img-container">
+            <img src="../assets/images/food-view-img-01.jpg" />
+          </div>
+          <div className="vfr-ttl-desc-container">
+            <h2>Rustic Crusty Bread</h2>
+            <p>These small but sophisticated blender cakes are not only eye-catching, but full of bright flavor. The rosemary syrup glaze is a must to complete the flavor profile.</p>
+          </div>
+          <div className="vfr-details-container">
+            <div className="vfr-details-block">
+              <p>
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-egg-fried" viewBox="0 0 16 16">
+                    <path d="M8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    <path d="M13.997 5.17a5 5 0 0 0-8.101-4.09A5 5 0 0 0 1.28 9.342a5 5 0 0 0 8.336 5.109 3.5 3.5 0 0 0 5.201-4.065 3.001 3.001 0 0 0-.822-5.216zm-1-.034a1 1 0 0 0 .668.977 2.001 2.001 0 0 1 .547 3.478 1 1 0 0 0-.341 1.113 2.5 2.5 0 0 1-3.715 2.905 1 1 0 0 0-1.262.152 4 4 0 0 1-6.67-4.087 1 1 0 0 0-.2-1 4 4 0 0 1 3.693-6.61 1 1 0 0 0 .8-.2 4 4 0 0 1 6.48 3.273z"/>
+                  </svg>
+                </span>
+              </p>  
+              <div className="vfr-details-content">
+                <span className="vfr-details-01">SERVINGS</span>  
+                <span className="vfr-details-02">
+                  <span>4</span>
+                  <span>people</span>
+                </span>
+              </div>
+            </div>
+            <div className="vfr-details-block">
+              <p>
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stopwatch" viewBox="0 0 16 16">
+                    <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+                    <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+                  </svg>
+                  </span>
+              </p>  
+              <div className="vfr-details-content">
+                <span className="vfr-details-01">PREP TIME</span>  
+                <span className="vfr-details-02">
+                  <span>10</span>
+                  <span>minutes</span>
+                </span>
+              </div>
+            </div>
+            <div className="vfr-details-block">
+              <p>
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stopwatch" viewBox="0 0 16 16">
+                    <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+                    <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+                  </svg>
+                </span>
+              </p>  
+              <div className="vfr-details-content">
+                <span className="vfr-details-01">COOK TIME</span>  
+                <span className="vfr-details-02">
+                  <span>20</span>
+                  <span>minutes</span>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="vfr-ingredients-container">
+            <h3>Ingredients</h3>
+            <div>
+              <ul className="vfr-ingredients-ul">
+                <li>
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 21 21">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                    </svg>
+                  </span>
+                  <span>8 </span><span>package (19 oz) </span><span class="vfr-ingre-name">queso brats</span>
+                </li>
+                <li>
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 21 21">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                    </svg>
+                  </span>
+                  <span>1 </span><span> </span><span class="vfr-ingre-name">small red onion, diced</span>
+                </li>
+                <li>
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 21 21">
+                      <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                    </svg>
+                  </span>
+                  <span>4 </span><span> </span><span class="vfr-ingre-name">corn tortillas, cut into 1/2-inch squares</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div> 
+      </div>
+      </>
+      
+    );
+  }
+  
+  export default viewRecipe;
